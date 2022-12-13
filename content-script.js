ee2730534614ea19976c2c68bc97b78dd64504dd
Alex Wulf <aw@dc.ag>
2022-12-14 00:00:30 +0100

    Working

---
+++ content-script.js
@@ -0,0 +1,12 @@
+
+console.log("Here")
+
+addEventListener('input', (event) => {
+    console.log('input')
+    console.log(event)
+})
+
+addEventListener('click', event => {
+    console.log('click')
+    console.log(event)
+})
